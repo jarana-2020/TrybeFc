afe8661b656577094ee7695b91daa00609d6c0a5
Julio <julioaranabr@yahoo.com.br>
2022-03-16 13:24:13 -0300

    created migrations

---
+++ app/backend/src/database/migrations/20220316012329-create-matchs.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+   await queryInterface.createTable('matchs', {
+     id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+     },
+     home_team: {
+      foreignKey: true,
+      type: Sequelize.INTEGER,
+      allowNull: false,
+     },
+     home_team_goals: {
+       type: Sequelize.INTEGER,
+       allowNull: false,
+     },
+     away_team: {
+      foreignKey: true,
+      type: Sequelize.INTEGER,
+      allowNull: false,
+     },
+     away_team_goals: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+     },
+     in_progress: {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+     }
+   })
+  },
+
+  down: async (queryInterface, _Sequelize) => {
+    await queryInterface.dropTable('matchs');
+  }
+};
