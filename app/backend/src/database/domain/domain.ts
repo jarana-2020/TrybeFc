1e5228f1ed16c709d0a88f3194111dc9c58a8aea
Julio <julioaranabr@yahoo.com.br>
2022-03-17 18:53:54 -0300

    token interface

--- app/backend/src/database/domain/domain.ts
+++ app/backend/src/database/domain/domain.ts
@@ -1,14 +1,25 @@
+import { Request } from 'express';
+
 export type UserRequest = {
   email: string,
-  password: string,
 };
 
 export interface UserData extends UserRequest {
   username: string,
   role: string,
   id: number,
+  password: string,
 }
 
 export type ErrorMessage = {
   message: string
 };
+
+export interface TokenData {
+  email: string,
+  iat: number,
+}
+
+export interface PersonalRequest extends Request {
+  email?: string;
+}
