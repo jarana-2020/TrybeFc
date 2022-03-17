37d3491d3beac5f0dc8121678129ee5889193a55
Julio <julioaranabr@yahoo.com.br>
2022-03-17 18:55:56 -0300

    refatored login service

--- app/backend/src/database/services/login.ts
+++ app/backend/src/database/services/login.ts
@@ -1,14 +1,32 @@
-import { ErrorMessage, UserRequest } from '../domain/domain';
+import * as bcrypt from 'bcryptjs';
+import { ErrorMessage } from '../domain/domain';
 import User from '../models/user';
 
-export class LoginService {
-  static async getUser(data: UserRequest) {
-    const { email, password } = data;
-    const user = await User.findOne({
-      where: { email, password },
+class LoginService {
+  static async getUser(email: string, password: string) {
+    const result = await User.findOne({
+      where: { email },
     });
+    const user = result as User;
+    const match = bcrypt.compareSync(password, user.password);
+    console.log('bcrypt', match);
+    console.log('result', result);
+    console.log('senhas', user.password, password);
+
     const message: ErrorMessage = { message: 'Incorrect email or password' };
-    if (!user) return message;
+    if (!result || !match) return message;
+    return {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+      email: user.email,
+    };
+  }
+
+  static async validateLogin(email: string) {
+    const user = await User.findOne({
+      where: { email },
+    });
     return user;
   }
 }
