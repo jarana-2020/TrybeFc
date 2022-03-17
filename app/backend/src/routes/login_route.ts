38d2d8466a3c892e5fb6e988373a8319e5e58631
Julio <julioaranabr@yahoo.com.br>
2022-03-17 18:57:25 -0300

    route /login/validate

--- app/backend/src/routes/login_route.ts
+++ app/backend/src/routes/login_route.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
 import validateEmail, { validatePassword } from '../database/middlewares/validateLogin';
 import { LoginController } from '../database/controllers/login';
+import validateToken from '../database/middlewares/validateToken';
 
 const loginRouter = express.Router();
 
 loginRouter
+  .get('/validate', validateToken, LoginController.get)
   .use(validateEmail)
   .use(validatePassword)
   .post('/', LoginController.add);
