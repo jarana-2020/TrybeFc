544e9b6f182a24fb6ac0fdf7c5cf2cf85a7348da
Julio <julioaranabr@yahoo.com.br>
2022-03-18 15:12:24 -0300

    service match by status

--- app/backend/src/database/services/matchs.ts
+++ app/backend/src/database/services/matchs.ts
@@ -11,6 +11,17 @@ class ServiceMatch {
     });
     return matchs;
   }
+
+  static async getByStatus(status: boolean) {
+    const matchs = await Match.findAll({
+      where: { inProgress: status },
+      include: [
+        { model: Club, as: 'homeClub', attributes: ['clubName'] },
+        { model: Club, as: 'awayClub', attributes: ['clubName'] },
+      ],
+    });
+    return matchs;
+  }
 }
 
 export default ServiceMatch;
