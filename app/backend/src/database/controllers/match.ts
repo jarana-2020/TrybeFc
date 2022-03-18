670763642b8d286600db68ecc6e0bd10c6a2bc46
Julio <julioaranabr@yahoo.com.br>
2022-03-18 15:12:40 -0300

    controller match by status

--- app/backend/src/database/controllers/match.ts
+++ app/backend/src/database/controllers/match.ts
@@ -3,8 +3,23 @@ import ServiceMatch from '../services/matchs';
 
 class MatchController {
   static async getAll(_req: Request, res: Response) {
-    const matchs = await ServiceMatch.getAll();
-    return res.status(200).json(matchs);
+    try {
+      const matchs = await ServiceMatch.getAll();
+      return res.status(200).json(matchs);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  }
+
+  static async getByStatus(req: Request, res: Response) {
+    try {
+      const { inProgress } = req.query;
+      const status = inProgress === 'true';
+      const matchs = await ServiceMatch.getByStatus(status);
+      return res.status(200).json(matchs);
+    } catch (error) {
+      return res.status(500).json(error);
+    }
   }
 }
 
