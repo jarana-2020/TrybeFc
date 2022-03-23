1d4e5451bded8b8a9da1af43aa07fff424355c80
Julio <julioaranabr@yahoo.com.br>
2022-03-23 15:56:31 -0300

    route leaderboard away

--- app/backend/src/routes/leaderboard_routes.ts
+++ app/backend/src/routes/leaderboard_routes.ts
@@ -1,9 +1,11 @@
 import * as express from 'express';
-import ControllerLeaderboard from '../database/controllers/leaderboards';
+import ControllerLeaderboardAway from '../database/controllers/leaderboardAway';
+import ControllerLeaderboard from '../database/controllers/leaderboardsHome';
 
 const leaderboardRouter = express.Router();
 
 leaderboardRouter
-  .get('/home', ControllerLeaderboard.getAllClubsAndMatchs);
+  .get('/home', ControllerLeaderboard.getAllClubsAndMatchs)
+  .get('/away', ControllerLeaderboardAway.getAllClubsAndMatchs);
 
 export default leaderboardRouter;
