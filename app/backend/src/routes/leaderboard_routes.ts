8b5a6b421e1dc89ff752dd74feb801f2930c7aec
Julio <julioaranabr@yahoo.com.br>
2022-03-24 09:22:45 -0300

    route general leaderboard

--- app/backend/src/routes/leaderboard_routes.ts
+++ app/backend/src/routes/leaderboard_routes.ts
@@ -1,10 +1,12 @@
 import * as express from 'express';
+import ControllerLeaderBoard from '../database/controllers/leaderboard';
 import ControllerLeaderboardAway from '../database/controllers/leaderboardAway';
 import ControllerLeaderboard from '../database/controllers/leaderboardsHome';
 
 const leaderboardRouter = express.Router();
 
 leaderboardRouter
+  .get('/', ControllerLeaderBoard.getClassification)
   .get('/home', ControllerLeaderboard.getAllClubsAndMatchs)
   .get('/away', ControllerLeaderboardAway.getAllClubsAndMatchs);
 
